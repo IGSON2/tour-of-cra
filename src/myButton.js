0b78feff3d4f3682e13bd5d2a98248f88e7ad72b
IGSON2 <suninnaa@gmail.com>
2022-03-18 21:41:42 +0900

    Chaper1

---
+++ src/myButton.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Btn({ text }) {
+  return <button className={styles.Btn}>{text}</button>;
+}
+
+//prop-Types 지정 (같은 방식)
+Btn.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Btn;
+
+//외부로 공개하려면 작성해 주어야 함.
+//'default' Arg는 '해당 모듈엔 개체가 하나만 있다'는 사실을 명시한다.
+//때문에 import시 {}중괄호 없이 모듈을 가져올 수 있게 된다.
